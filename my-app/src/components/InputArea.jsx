768ede2bc06ac2e705850202fa031d982b7031e5
Shishir <kushwahashishir1112@gmail.com>
2023-12-14 16:43:18 +0530

    Content Creation final

--- my-app/src/components/InputArea.jsx
+++ my-app/src/components/InputArea.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React,{ useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from "@mui/material/Zoom";
+
 
 function InputArea(props) {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    function expand() {
+      setIsExpanded(true);  
+    }
 
     return (<div>
         <form onSubmit={(event)=>event.preventDefault()}>
-            <input type="text" placeholder="Title" onChange={props.onChange} value={props.value.title} name="title"></input>
-            <input type="text" placeholder="Take a note..." onChange={props.onChange} value={props.value.content} name="content"></input>
-            <button type="submit" onClick={props.onAdd}>Add</button>
+            {isExpanded && <input type="text" placeholder="Title" onChange={props.onChange} value={props.value.title} name="title"></input>}
+            <textarea type="text" placeholder="Take a note..." onChange={props.onChange} value={props.value.content} name="content" onClick={expand} rows={isExpanded?3:1}></textarea>
+            <Zoom in={isExpanded}><Fab type="submit" onClick={props.onAdd} className="button"><AddIcon/></Fab></Zoom>
 
         </form>
     </div>);
