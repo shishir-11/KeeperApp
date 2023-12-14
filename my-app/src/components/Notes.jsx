04df68995ff784e973832ed7f072cc33902e4c9f
Shishir <kushwahashishir1112@gmail.com>
2023-12-14 15:56:08 +0530

    Add Delete icon

--- my-app/src/components/Notes.jsx
+++ my-app/src/components/Notes.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Notes(props) {
     return (<div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-        <button onClick={()=>{props.onDelete(props.id)}}>DELETE</button>
+        <button onClick={()=>{props.onDelete(props.id)}}><DeleteIcon /></button>
     </div>);
 }
 
